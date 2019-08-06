9ccc72e7acb677f26a1994f7331dae5312624a26
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-08-06 10:26:51 -0400

    Removing Entry

--- matisse3g-vendor.mk
+++ matisse3g-vendor.mk
@@ -23,7 +23,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/matisse3g/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/samsung/matisse3g/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
     vendor/samsung/matisse3g/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
-    vendor/samsung/matisse3g/proprietary/lib/librilutils.so:system/lib/librilutils.so \
     vendor/samsung/matisse3g/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
     vendor/samsung/matisse3g/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/matisse3g/proprietary/lib/hw/vibrator.default.so:lib/hw/vibrator.default.so \
